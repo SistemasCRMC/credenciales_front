78cce1401469ce3fdd9d2ddf93bedb3596bf2601
SistemasCRMC <sistemas@cruzrojacancun.org>
2025-08-02 12:38:05 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-server {
-    listen 8081;  # Usamos un puerto distinto porque el 80 ya está ocupado
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/credenciales',
+  output: 'standalone',
+  trailingSlash: true, // (opcional pero ayuda con recursos estáticos)
+};
 
-    server_name credenciales.cruzrojacancun.org;
-
-    location /credenciales/ {
-        proxy_pass http://10.0.1.12:3000/;  # Aquí la IP interna del contenedor Docker
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-    }
-}
+module.exports = nextConfig;
