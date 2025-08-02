6cbb66717d1293bb4d42073e45b84eee8d517835
SistemasCRMC <sistemas@cruzrojacancun.org>
2025-08-01 22:15:31 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/credenciales', // Esto le dice a Next.js que maneje la app bajo este path
+}
+
+module.exports = nextConfig
